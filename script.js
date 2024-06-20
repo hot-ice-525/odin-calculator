3065db6a3d7d97b9c40b6a435ae59be5eb3c3ccc
lalit-machra <lalitmachra0403@gmail.com>
2024-06-20 17:20:31 +0530

    Add functionality of clear button

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log("Hii");
+function add(firstNum, secondNum) {
+  return (firstNum + secondNum);
+}
+
+function subtract(firstNum, secondNum) {
+  return (firstNum - secondNum);
+}
+
+function multiply(firstNum, secondNum) {
+  return (firstNum * secondNum);
+}
+
+function divide(firstNum, secondNum) {
+  return (firstNum / secondNum);
+}
+
+let firstNumber,
+    operator,
+    secondNumber;
+
+function operate(firstNum, operator, secondNum) {
+  if (operator === "+") {
+    return add(firstNum, secondNum);
+  }
+  else if (operator === "-") {
+    return subtract(firstNum, secondNum);
+  }
+  else if (operator === "*") {
+    return multiply(firstNum, secondNum);
+  }
+  else if (operator === "/") {
+    return divide(firstNum, secondNum);
+  }
+  else {
+    return undefined;
+  }
+}
+
+const displayAnswer = document.querySelector(".display > .answer");
+
+const btn = document.querySelector(".buttons");
+btn.addEventListener("click", (e) => {
+  const btnValue = e.target.value;
+  if (btnValue === "AC") {
+    displayAnswer.innerHTML = "";
+  }
+})
