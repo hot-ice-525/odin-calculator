ac73744aaeb9fa5e1920cac4fcb992bfc432fa04
lalit-machra <lalitmachra0403@gmail.com>
2024-06-21 06:43:02 +0530

    Fix button press leads to "undefined" value
    There was this bug that lead to printing "undefined" to the display. It
    was caused due to the fact that clicks were registered from the divs
    which were enclosing the buttons i.e. ".row-1", ".row-2", etc, because
    the querySelector was applied on ".buttons" div so that it would capture
    events upon bubbling two levels up. Now, the event listeners are applied
    on the buttons themselves so this minor possibility of their parent divs
    getting clicked and registered is diminished.

--- script.js
+++ script.js
@@ -39,18 +39,20 @@ function operate(firstNum, operator, secondNum) {
 const displayAnswer = document.querySelector(".display > .answer");
 let equation = null;
 
-const btn = document.querySelector(".buttons");
-btn.addEventListener("click", (e) => {
-  const btnValue = e.target.value;
-  if (btnValue === "AC") {
-    displayAnswer.textContent = "";
-    equation = displayAnswer.textContent;
-  }
-  else if (btnValue === "=") {
-    console.log(equation);
-  }
-  else {
-    displayAnswer.textContent += btnValue;
-    equation = displayAnswer.textContent;
-  }
-})
+const allBtns = document.querySelectorAll(".calc-button");
+allBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const btnValue = e.target.value;
+    if (btnValue === "AC") {
+      displayAnswer.textContent = "";
+      equation = displayAnswer.textContent;
+    }
+    else if (btnValue === "=") {
+      console.log(equation);
+    }
+    else {
+      displayAnswer.textContent += btnValue;
+      equation = displayAnswer.textContent;
+    }
+  });
+});
